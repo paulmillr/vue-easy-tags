91927f7db47ef77379bfb23fa9dce2e735bdbf8b
Steve Shreeve <steve.shreeve@gmail.com>
2016-11-03 01:52:39 -0400

    bump to 0.3.0

--- index.js
+++ index.js
@@ -1,76 +1,83 @@
-var reserved = 'attrs directives domProps key nativeOn on props ref slot style'.split(' ').reduce(function (reserved, key) {
+var reserved = 'attrs class directives domProps key nativeOn on props ref slot staticClass style'.split(' ').reduce(function (reserved, key) {
   reserved[key] = true;
   return reserved;
 }, {})
+
+function toArray (list, start) {
+  start = start || 0;
+  var i = list.length - start;
+  var ret = new Array(i);
+  while (i--) {
+    ret[i] = list[i + start];
+  }
+  return ret;
+}
 
 module.exports = function (
   tag,
   data,
   children
 ) {
-  var len = arguments.length;
-  var str = typeof tag === 'string';
+  var pos = 1;
 
-  // check for and process enhanced render arguments
-  if (len > 3 || tag === '' || (str && (tag.charAt(0) === '.' || tag.match(/[.@!?]/))) || (data && typeof data === 'object')) {
-    var chr;
-    var raw;
-    var dbg;
-    var ary = Array.apply(null, arguments);
+  // process boolean short circuit
+  if (typeof data === 'boolean') {
+    if (!data) { return; }
+    data = children;
+    children = arguments[3];
+    pos++;
+  }
+
+  // adjust arguments
+  if (data) {
+    if (Array.isArray(data) || typeof data !== 'object') {
+      children = data;
+      data = undefined;
+    } else if (data.constructor.name === 'VNode') {
+      children = toArray(arguments, pos);
+      data = undefined;
+    }
+  }
+  if (children && typeof children === 'object' && children.constructor.name === 'VNode') {
+    children = toArray(arguments, pos + 1);
+  }
+
+  // process tag modifiers
+  if (tag === '' || typeof tag === 'string' && tag.match(/[.@!?]/)) {
     var cls = [];
     var eid;
+    var raw;
+    var dbg;
 
-    // parse tag for class, id, or trailing "!" (unsafe HTML) or "?" (debug mode)
-    tag = ary.shift();
-    if (str) {
-      len = tag.length;
-      chr = tag.charAt(len - 1);
-      if (chr === '!' || chr === '?') {
-        chr === '!' ? (raw = true) : (dbg = true);
-        tag = tag.slice(0, len - 1);
-      }
+    // check tag for trailing "!" (raw HTML) or "?" (debug mode)
+    var len = tag.length;
+    var chr = tag.charAt(len - 1);
+    if (chr === '!' || chr === '?') {
+      chr === '!' ? (raw = true) : (dbg = true);
+      tag = tag.slice(0, len - 1);
+    }
 
-      // check for tag, class, and id values
-      var all = tag.split(/([.@])/);
-      tag = all[0] || 'div';
-      len = all.length;
-      for (var pos = 0; ++pos < len;) {
-        if (all[pos++].charAt(0) === '.') {
-          cls.push(all[pos]);
-        } else {
-          eid = all[pos];
-        }
+    // check tag for classes and id
+    var all = tag.split(/([.@])/);
+    tag = all[0] || 'div';
+    len = all.length;
+    for (var pos = 0; ++pos < len;) {
+      if (all[pos++].charAt(0) === '.') {
+        cls.push(all[pos]);
+      } else {
+        eid = all[pos];
       }
     }
 
-    // process obj
-    var obj = ary.shift();
-    str = obj != null ? obj.constructor.name : null;
-    if (str === 'Object') {
-      obj.attrs = obj.attrs || {};
-      Object.keys(obj).forEach(function (key) {
-        if (!reserved[key]) {
-          obj.attrs[key] = obj[key];
-          delete obj[key];
-        }
-      });
-    } else if (str === 'Boolean') {
-      if (!obj) { return }
-      obj = undefined;
-    } else {
-      ary.unshift(obj);
-      obj = undefined;
-    }
-
-    // set easy-tags properties
+    // set tag properties
     if (cls || eid || raw || dbg) {
-      if (!obj) { obj = {}; }
-      if (cls) { obj.staticClass = cls.join(' '); }
-      if (eid) { obj.attrs = obj.attrs || {}; obj.attrs.id = eid; }
-      if (raw) { obj.domProps = { innerHTML: ary.shift() }; }
+      if (!data) { data = {}; }
+      if (cls.length) { if (data.staticClass) { cls.unshift(data.staticClass) }; data.staticClass = cls.join(' '); }
+      if (eid) { if (!data.attrs) { data.attrs = { id: eid } } else { data.attrs.id = eid } }
+      if (raw) { data.domProps = { innerHTML: children }; children = undefined; }
       if (dbg) {
-        tag = 'pre'; // override tag?
-        obj.style = {
+        tag = 'pre';
+        data.style = {
           'margin': '0',
           'border': '1px solid red',
           'padding': '10px',
@@ -78,24 +85,31 @@ module.exports = function (
           'background': '#fcfcb8',
           'line-height': '16px',
           'font-size': '12px',
-          'white-space': 'pre-line'
+          'white-space': 'pre-line',
+          'overflow': 'auto'
         };
-        const spy = function spy (obj) { }; // add function to deep-dive into children...
-        ary = (Array.isArray(ary[0]) ? ary[0] : ary).map(function(val) {
+        // const spy = function spy (data) { }; // add function to deep-dive into children...
+        children = (Array.isArray(children[0]) ? children[0] : children).map(function(val) {
           return `\n[${val.tag || val.text || JSON.stringify(val)}, ${JSON.stringify(val.data)}]\n`;
         });
-        ary.unshift("Debug:\n");
+        children.unshift("Debug:\n");
       }
     }
-
-    // invoke virtual dom
-    return Vue._createElement(this._self, tag, obj, ary)
   }
 
-  if (data && (Array.isArray(data) || typeof data !== 'object')) {
-    children = data;
-    data = undefined;
+  // process data object
+  if (data && typeof tag === 'string') {
+    Object.keys(data).forEach(function (key) {
+      if (data.hasOwnProperty(key)) {
+        if (!reserved[key]) {
+          if (!data.attrs) { data.attrs = {}; }
+          data.attrs[key] = data[key];
+          delete data[key];
+        }
+      }
+    });
   }
+
   // make sure to use real instance instead of proxy as context
   return Vue._createElement(this._self, tag, data, children)
 }
